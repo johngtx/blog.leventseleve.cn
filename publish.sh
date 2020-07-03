400cee2e413cab0fc11545c5570dbbc53612eb0c
xiongpengchao <xiongpengchao@chinawebox.com>
2020-07-03 23:47:54 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "Update pulic contents"
+
+cd /usr/local/leventseleve
+
+if [ ! -d "www" ]; then
+    git clone https://github.com/johngtx/leventseleve.cn ./www
+fi
+
+cd www
+git pull
+
 hugo --theme=even --baseUrl="http://leventseleve.cn/"
